74c40dfc61c57880e929fcef9b4975dc733cadf8
你的使用者名稱 <>
2024-03-08 14:54:36 +0800

    initialize_1

--- Source/Game/mygame_initialize.cpp
+++ Source/Game/mygame_initialize.cpp
@@ -25,8 +25,9 @@ void CGameStateInit::OnInit()
 	ShowInitProgress(0, "Start Initialize...");	// 一開始的loading進度為0%
 	//
 	// 開始載入資料
+	load_background();
 	//
-	Sleep(1000);				// 放慢，以便看清楚進度，實際遊戲請刪除此Sleep
+	//Sleep(1000);				// 放慢，以便看清楚進度，實際遊戲請刪除此Sleep
 	//
 	// 此OnInit動作會接到CGameStaterRun::OnInit()，所以進度還沒到100%
 	//
@@ -46,6 +47,64 @@ void CGameStateInit::OnLButtonDown(UINT nFlags, CPoint point)
 	GotoGameState(GAME_STATE_RUN);		// 切換至GAME_STATE_RUN
 }
 
+
 void CGameStateInit::OnShow()
 {
+	background.ShowBitmap();
+	character.ShowBitmap();
+	ball.ShowBitmap();
+	start.ShowBitmap();
+
+
+	for (int i = 0; i < 2; i++) {
+		if (character.GetLeft() == 300) {
+			char_flag = 1;
+		}
+		else if (character.GetLeft() == 80) {
+			char_flag = 0;
+		}
+
+		if (char_flag == 0) {
+			character.SetTopLeft(character.GetLeft() + 1, character.GetTop());
+		}
+		if (char_flag == 1) {
+			character.SetTopLeft(character.GetLeft() - 1, character.GetTop());
+		}
+	}
+
+	for (int i = 0; i < 5; i++) {
+		if (ball.GetTop() == 535) {
+			ball_flag = 1;
+		}
+		else if (ball.GetTop() == 470) {
+			ball_flag = 0;
+		}
+
+		// 模擬球的彈跳
+		if (ball_flag == 0) {
+			// 讓球向下移動
+			ball.SetTopLeft(ball.GetLeft(), ball.GetTop() + 1);
+
+		}
+		else if (ball_flag == 1) {
+			// 讓球向上移動
+			ball.SetTopLeft(ball.GetLeft(), ball.GetTop() - 1);
+		}
+	}
+
+
+}
+
+void CGameStateInit::load_background() {
+	background.LoadBitmapByString({ "resources/aa.bmp" });
+	background.SetTopLeft(0, 0);
+
+	character.LoadBitmapByString({ "resources/char_init.bmp"},RGB(0, 0, 0));
+	character.SetTopLeft(130, 130); 
+
+	ball.LoadBitmapByString({ "resources/ball.bmp" },RGB(0,0,0));
+	ball.SetTopLeft(216, 531);
+
+	start.LoadBitmapByString({ "resources/play-button.bmp" }, RGB(0, 0, 0));
+	start.SetTopLeft(143, 548);
 }
